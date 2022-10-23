b46d77e06c910d40fd7b92c59c42be8b468c118c
K-ash-ish <kashishsondhiya@gmail.com>
2022-10-23 11:17:40 +0530

    initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+function Header(){
+    return(
+        <header><h1>Keeper App</h1></header>
+    );
+}
+export default Header;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,9 @@
+function Notes(){
+    return(
+        <div className="note">
+            <h1>New Title</h1>
+            <p>New Body</p>
+        </div>
+    )
+}
+export default Notes;
